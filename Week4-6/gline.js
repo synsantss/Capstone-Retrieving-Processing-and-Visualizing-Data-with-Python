c33446977486e274163bed9e5e2eb3d4b6470d3c
Marco Antonio <marcochel@gmail.com>
2017-11-13 14:30:05 -0300

    Add files via upload

---
+++ Week4-6/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Year','mtu.edu','cam.ac.uk','unicon.net','virginia.edu','asu.edu','gmail.com','indiana.edu','suny.edu','columbia.edu','ucdavis.edu'],
+['2005-12',2,2,2,2,1,1,1,1,1,1]
+];
